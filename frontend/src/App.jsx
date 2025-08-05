55fb434ceed46c875d3359f83724f8d019646b3f
kimsongie12 <song3078@naver.com>
2025-08-05 16:00:46 +0900

    refator: 경로수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyAsset_Button from "/src/components/MyAsset_Button";
 import Login from "./components/login/Login";
-import FindIdPage from "./components/login/FindIdPage";
 import ResetPasswordPage from "./components/login/ResetPasswordPage";
 import SignUp from "./components/login/SignUp";
 
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<MyAsset_Button />} />
         <Route path="/login" element={<Login />} /> {/* 로그인 라우트 추가 */}
-        <Route path="/find-id" element={<FindIdPage />} /> {/* 아이디 찾기 추가 */}
         <Route path="/reset-password" element={<ResetPasswordPage />} /> {/* 비밀번호찾기 추가 */}
         <Route path="/signup" element={<SignUp />} /> {/* 회원가입 추가 */}
       </Routes>
