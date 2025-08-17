2418a41da8580a346f6330e523edd7efc2accdd1
KyuhwanPark <parkkh9989@gmail.com>
2025-08-17 12:36:55 +0900

    수리기사 전체,고객사별 목록 조회

--- backend/src/main/java/com/example/asplatform/engineer/repository/EngineerRepository.java
+++ backend/src/main/java/com/example/asplatform/engineer/repository/EngineerRepository.java
@@ -1,8 +1,11 @@
 package com.example.asplatform.engineer.repository;
 
 import com.example.asplatform.engineer.domain.Engineer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface EngineerRepository extends JpaRepository<Engineer, Long> {
+    Page<Engineer> findByCustomerId(Long customerId, Pageable pageable);
 }
